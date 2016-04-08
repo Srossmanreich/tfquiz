ac368a40717cce925a3031e598e5e65c6c27a3a2
Mihai Banulescu <mihai.banulescu@gmail.com>
2016-04-08 10:27:19 -0700

    /quiz/topic/given_topic path implemented

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,7 @@
 class VisitorsController < ApplicationController
+
+  def index
+    @topic = Topic.where(name: params[:topic]).try(:first)
+    @questions = Question.by_topic(@topic).randomized.shuffle
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,24 @@
 class Question < ApplicationRecord
   belongs_to :topic
+
+  def present_random
+    current_choice ? t : f
+  end
+
+  def peek
+    "Correct answer: #{current_choice ? 'True' : 'False'}."
+  end
+
+  def randomize
+    update(current_choice: [true, false].sample)
+  end
+
+  def self.randomized
+    Question.all.each(&:randomize)
+  end
+
+  def self.by_topic(given_topic)
+    given_topic ? where(topic: given_topic) : Question.all
+  end
+
 end
